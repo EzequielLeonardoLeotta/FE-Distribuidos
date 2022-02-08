67816946b6fcddc3e4317224eba6d74713c22dc9
pablo miranda <pablomiranda@pablos-MacBook-Pro.local>
2022-02-08 01:13:49 -0300

    d

--- src/pages/MesaDeAyuda.tsx
+++ src/pages/MesaDeAyuda.tsx
@@ -82,7 +82,7 @@ const MesaDeAyuda: React.FC<{}> = () => {
      getProductos();
    },[])
    
-   
+   // HOLA PRUEBA 2
    const getReclamos = () => {
      fetch("http://localhost:8081/api/v1/reclamo", { method: "GET" })
        .then((response) => response.json())
